c1b15095f37bc785219896e4ed2d4744b65b02ac
RevolverSnake412 <oussag@hotmail.fr>
2023-10-20 21:02:55 +0100

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Repeatedly select the minimum element and swap.
+ * @array: The provided array to sort.
+ * @size: The size of the array.
+*/
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int smallest;
+	int idx;
+	int flag;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		smallest = array[i];
+		flag = 0;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < smallest)
+			{
+				smallest = array[j];
+				idx = j;
+				flag = 1;
+			}
+		}
+
+		if (flag == 1)
+		{
+			array[idx] = array[i];
+			array[i] = smallest;
+			print_array(array, size);
+		}
+	}
+}
