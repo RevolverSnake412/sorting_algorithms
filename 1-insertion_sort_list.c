2d99aef3779ed9c5898e1a392141aa2f82e53af7
logmar98 <marwanarbib15@gmail.com>
2023-10-24 00:23:39 +0100

    old code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,58 +1,107 @@
 #include "sort.h"
 
-/**
- * swap - Swap a node with the next node
- * @node: The node to be swapped with its next node
- */
-void swap(listint_t *node)
-{
-    if (node == NULL || node->next == NULL)
-        return;
 
-    listint_t *tmp = node->next;
-
-    if (tmp->next != NULL)
-        tmp->next->prev = node;
-
-    if (node->prev != NULL)
-        node->prev->next = tmp;
-
-    node->next = tmp->next;
-    tmp->prev = node->prev;
-    tmp->next = node;
-    node->prev = tmp;
-}
+void swap(listint_t *list);
+listint_t *get_first(listint_t *list);
 
 /**
- * insertion_sort_list - Sort a linked list using the insertion sort algorithm
- * @list: Pointer to the doubly linked list
+ * insertion_sort_list - sort a linked list in insertion algorithme
+ *
+ * @list: Pointer to the doubly linked list.
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (!list || !*list || !(*list)->next)
-        return;
+	listint_t *tmp;
+	listint_t *cur, *first;
 
-    listint_t *current = *list;
+	cur = *list;
+	if (*list == NULL || list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
 
-    while (current)
-    {
-        if (current->n > current->next->n)
-        {
-            listint_t *tmp = current;
-            swap(tmp);
+	while (cur != NULL)
+	{
+		if (cur->next == NULL)
+		{
+			(*list) = get_first((*list));
+			return;
+		}
+		if (cur->n == cur->next->n)
+		{
+			cur = cur->next;
+		}
+		else if (cur->n < cur->next->n)
+		{
+			cur = cur->next;
+		}
+		else if (cur->n > cur->next->n)
+		{
+			tmp = cur;
+			swap(tmp);
+			tmp = tmp->prev;
+			first = get_first(cur);
+			print_list(first);
+			while (tmp != NULL)
+			{
+				if (tmp->prev == NULL || tmp->n > tmp->prev->n)
+				{
+					break;
+				}
+				else if (tmp->n < tmp->prev->n)
+				{
+					swap(tmp->prev);
+					first = get_first(*list);
+					print_list(first);
+				}
+			}
+		}
+	}
+}
+/**
+ * swap - swap a node with the next node
+ *
+ * @list: Pointer to the node that will be swap
+ */
+void swap(listint_t *list)
+{
+	listint_t *tmp;
 
-            while (tmp->prev && tmp->n < tmp->prev->n)
-            {
-                swap(tmp->prev);
-                tmp = tmp->prev;
-            }
+	if (list == NULL || list->next == NULL)
+	{
+		return;
+	}
+	tmp = list->next;
+	if (tmp->next != NULL)
+	{
+		tmp->next->prev = list;
+	}
+	if (list->prev != NULL)
+	{
+		list->prev->next = tmp;
+	}
 
-            if (!tmp->prev)
-                *list = tmp;
-        }
-        else
-        {
-            current = current->next;
-        }
-    }
+	list->next = tmp->next;
+	tmp->prev = list->prev;
+	tmp->next = list;
+	list->prev = tmp;
+}
+
+/**
+ * get_first - get to the first node of a double linked list
+ *
+ * @list: Pointer to a node
+ *
+ * Return: first node
+ */
+listint_t *get_first(listint_t *list)
+{
+	listint_t *head;
+
+	head = list;
+	while (head->prev != NULL)
+	{
+		head = head->prev;
+	}
+	return (head);
 }
