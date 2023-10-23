0d89145545176195300fa8919be95bcd1f0040ba
RevolverSnake412 <oussag@hotmail.fr>
2023-10-23 16:31:55 +0100

    Seperating functions

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,8 @@
 #include "sort.h"
 
+int first_rule(int *array, size_t size, size_t *i, size_t *j, int *pivot);
+int second_rule(int *array, size_t size, size_t *i, size_t *j, int *pivot);
+
 /**
  * quick_sort - Sorts an array of integers in ascending order
  *				using the Quick Sort algorithm.
@@ -10,7 +13,6 @@ void quick_sort(int *array, size_t size)
 {
 	size_t i = 1, j = 0;
 	int pivot = array[size - i];
-	int current = array[j];
 	int rule = 0;
 
 	/*
@@ -28,49 +30,71 @@ void quick_sort(int *array, size_t size)
 		/*The first rule of Quick sort*/
 		if (rule == 0)
 		{
-			if (current <= pivot)
-			{
-				/*Shift the pv to the left*/
-				i++;
-				pivot = array[size - i];
-			}
-			else if (current > pivot)
-			{
-				/*Doing the swaping between pv and curr*/
-				array[j] = pivot;
-				array[size - i] = current;
-
-				print_array(array, size);
-				/*Shift the pv to the right*/
-				j++;
-				pivot = array[j];
-
-				rule = 1;
-			}
+			rule = first_rule(array, size, &i, &j, &pivot);
 		}
 
 		/*The second rule of Quick sort*/
 		if (rule == 1)
 		{
-			if (pivot < current)
-			{
-				/*Shift the pv to the right*/
-				j++;
-				pivot = array[j];
-			}
-			else if (pivot > current)
-			{
-				/*Doing the swaping between pv and curr*/
-				array[j] = current;
-				array[size - i] = pivot;
-				print_array(array, size);
-
-				/*Shift the pv to the left*/
-				i++;
-				pivot = array[size - i];
-
-				rule = 0;
-			}
+			rule = second_rule(array, size, &i, &j, &pivot);
 		}
 	}
+}
+
+/**
+ * first_rule - Called Initially or after a second swap.
+ * @array: the array to sort.
+ * @size: the size of the array.
+ * @i: index selector from the end.
+ * @j: index selector from the beginning.
+ * @pivot: the pivot's value.
+*/
+int first_rule(int *array, size_t size, size_t *i, size_t *j, int *pivot)
+{
+    int current = array[*j];
+	if (current <= *pivot)
+	{
+		(*i)++;
+		*pivot = array[size - (*i)];
+	}
+	else if (current > *pivot)
+	{
+		array[*j] = *pivot;
+		array[size - (*i)] = current;
+		print_array(array, size);
+		(*j)++;
+		*pivot = array[*j];
+		return 1;
+	}
+	return 0;
+}
+
+/**
+ * second_rule - Called after the initial swap.
+ * @array: the array to sort.
+ * @size: the size of the array.
+ * @i: index selector from the end.
+ * @j: index selector from the beginning.
+ * @pivot: the pivot's value.
+*/
+int second_rule(int *array, size_t size, size_t *i, size_t *j, int *pivot)
+{
+    int current = array[size - (*i)];
+	if (*pivot < current)
+	{
+		(*j)++;
+		*pivot = array[*j];
+	}
+	else if (*pivot > current)
+	{
+		array[*j] = current;
+		array[size - (*i)] = *pivot;
+
+		print_array(array, size);
+
+		(*i)++;
+		*pivot = array[size - (*i)];
+		return 0;
+	}
+	return 1;
 }
