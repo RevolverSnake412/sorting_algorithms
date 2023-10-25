984968a4de659d52c35eef0d002a95456dfabf8b
RevolverSnake412 <oussag@hotmail.fr>
2023-10-25 01:48:01 +0100

    Downgrade

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,10 +1,5 @@
 #include "sort.h"
 
-void new_split(int *new_arr, size_t size, int *pv, int *cur);
-void get_pv(int *new_arr, size_t size,int *pv);
-void get_cur(int *new_arr, int *cur);
-
-
 /**
  * quick_sort - Sorts an array of integers in ascending order
  *				using the Quick Sort algorithm.
@@ -13,109 +8,69 @@ void get_cur(int *new_arr, int *cur);
 */
 void quick_sort(int *array, size_t size)
 {
-	int *new_arr = malloc(size * sizeof(int));
-	int cur = 0;
-	int swap = -1;
-	int pv = size - 1;
-	int tmp;
-	int pv_tmp = pv;
+	size_t i = 1, j = 0;
+	int pivot = array[size - i];
+	int current = array[j];
+	int rule = 0;
 
-
-	while (cur != 11)
+	/*
+		* Currently using an infinite while loop,
+		* don't worry about it.
+	*/
+	while (1)
 	{
-		if (array[cur] > array[pv])
-			cur++;
-		else if (array[cur] <= array[pv])
+		/*The if statement when pivot and current will be on the same item*/
+		if (i + j == size)
 		{
-			
-			swap++;
-			if (array[cur] < array[swap])
+			return;
+		}
+
+		/*The first rule of Quick sort*/
+		if (rule == 0)
+		{
+			if (current <= pivot)
 			{
-				tmp = array[cur];
-				array[cur] = array[swap];
-				array[swap] = tmp;
+				/*Shift the pv to the left*/
+				i++;
+				pivot = array[size - i];
+			}
+			else if (current > pivot)
+			{
+				/*Doing the swaping between pv and curr*/
+				array[j] = pivot;
+				array[size - i] = current;
+
+				print_array(array, size);
+				/*Shift the pv to the right*/
+				j++;
+				pivot = array[j];
+
+				rule = 1;
+			}
+		}
+
+		/*The second rule of Quick sort*/
+		if (rule == 1)
+		{
+			if (pivot < current)
+			{
+				/*Shift the pv to the right*/
+				j++;
+				pivot = array[j];
+			}
+			else if (pivot > current)
+			{
+				/*Doing the swaping between pv and curr*/
+				array[j] = current;
+				array[size - i] = pivot;
 				print_array(array, size);
 
-				if (cur == pv)
-				{
-					pv = swap;
-					new_arr[swap] = swap + 1;
-					//print_array(new_arr, size);
-					if (pv != pv_tmp)
-					{
-						new_split(new_arr, size, &pv, &cur);
-						
-					}
-				}
+				/*Shift the pv to the left*/
+				i++;
+				pivot = array[size - i];
+
+				rule = 0;
 			}
-			else if (array[cur] == array[swap])
-				cur++;	
-		}		
-	}
-}
-
-void new_split(int *new_arr, size_t size, int *pv, int *cur)
-{
-	int k = size;
-	int flag = 0;
-	int tmp;
-	if ((*pv) - 1 >= 0 && new_arr[(*pv) - 1] == 0)
-	{
-		(*pv)--;
-		*cur = *pv;
-
-		printf("hello from left %d\n", (*pv));
-		get_cur(new_arr, cur);
-		printf("\n%d\n", *cur);
-		if ((*cur) == (*pv))
-		{
-			new_arr[(*cur)] = (*cur) + 1;	
 		}
-		else
-		{
-			flag = 1;
-		}
-		
-	}
-	if ((*pv) + 1 < k && new_arr[(*pv) + 1] == 0)
-	{
-		if (flag == 1)
-		{
-			tmp = *pv + 1;
-		}
-		else
-		{
-			(*pv)++;
-			*cur = *pv;
-		}
-
-		printf("hello from right %d\n", (*pv));
-		get_pv(new_arr, size, pv);
-		
-	}
-	else
-	{
-		*cur = 11;
-	}
-	
-	
-	
-}
-
-
-void get_pv(int *new_arr, size_t size,int *pv)
-{
-	int k = size;
-	while ((*pv) + 1 < k && new_arr[(*pv) + 1] == 0)
-	{
-		(*pv)++;
-	}
-}
-
-void get_cur(int *new_arr, int *cur)
-{
-	while ((*cur) - 1 >= 0 && new_arr[(*cur) - 1] == 0)
-	{
-		(*cur)++;
 	}
 }
